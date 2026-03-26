ed0174a397fba92e8648b80254175453ae5b2617
Abiodun Agbaji (ABTECH) <abiodune752@gmail.com>
2026-03-27 00:55:28 +0100

    Final changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 //body parser middleware
 app.use(express.json());
 
+
 let todos = [
   { id: 1, task: 'Learn Node.js', completed: false },
   { id: 2, task: 'Build CRUD API', completed: false },
